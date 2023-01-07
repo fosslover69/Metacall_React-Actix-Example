8f7dba474769c58fc6fbb2c20b320d3b632c3105
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-01-07 20:48:29 +0530

    App.tsx: Add TailwindCSS

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import "./App.css";
 
 export function hello(text: string):	string {
-	return renderToString(<h1 className="main">Hello {text}</h1>);
+	return renderToString(
+	<div>
+		<script src="https://cdn.tailwindcss.com"></script>
+		<h1 className="text-4xl font-bold">Hello {text}</h1></div>);
 }
